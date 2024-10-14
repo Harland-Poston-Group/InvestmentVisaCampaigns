c13f1200b62df54a0ab17077f7668c8a0981930b
PauloAPBernardes <paulo.bernardes@portugalhomes.com>
2024-10-14 14:43:51 +0100

    form sub

--- public/form-submission.php
+++ public/form-submission.php
@@ -1,5 +1,6 @@
 <?php
 // File to handle the form submission and create the lead within Dynamics
+use Illuminate\Support\Facades\Mail;
 
 // Include Composer's autoload file
 require '../vendor/autoload.php';
@@ -16,16 +17,19 @@ $response = $kernel->handle(
     $request = Illuminate\Http\Request::capture()
 );
 
-if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-    echo 'IP address = ' . $_SERVER['HTTP_CLIENT_IP'];
-} //if user is from the proxy
+if(!empty($_SERVER['HTTP_CLIENT_IP'])) {
+    echo 'IP address = '.$_SERVER['HTTP_CLIENT_IP'];
+}
+//if user is from the proxy
 elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-    echo 'IP address = ' . $_SERVER['HTTP_X_FORWARDED_FOR'];
-} //if user is from the remote address
-else {
-    echo 'IP address = ' . $_SERVER['REMOTE_ADDR'];
+    echo 'IP address = '.$_SERVER['HTTP_X_FORWARDED_FOR'];
+}
+//if user is from the remote address
+else{
+    echo 'IP address = '.$_SERVER['REMOTE_ADDR'];
 }
 
+// echo 'this is running';
 
 // Configuration
 $apiUrl = getenv('DYNAMICS_API_URL'); // Replace with your API URL
@@ -34,80 +38,87 @@ $clientId = getenv('DYNAMICS_CLIENT_ID'); // Replace with your client ID
 $clientSecret = getenv('DYNAMICS_CLIENT_SECRET'); // Replace with your client secret
 $resource = getenv('DYNAMICS_RESOURCE'); // Replace with your resource URL
 
-if (isset($_POST['petname']) && !empty($_POST['petname'])) {
+if( isset($_POST['petname']) && !empty($_POST['petname']) ){
     $petname = $_POST['petname'];
 }
-
-
-// echo '<pre>';
-// print_r($apiUrl);
-// echo '<br>';
-// print_r($tokenUrl);
-// echo '<br>';
-// print_r($clientId);
-// echo '<br>';
-// print_r($clientSecret);
-// echo '<br>';
-// print_r($resource);
-// echo '</pre>';
-
-// die('hey');
-
-if (isset($_POST['first_name']) && !empty($_POST['first_name'])) {
+/*
+ echo '<pre>';
+ print_r($apiUrl);
+ echo '<br>';
+ print_r($tokenUrl);
+ echo '<br>';
+ print_r($clientId);
+ echo '<br>';
+ print_r($clientSecret);
+ echo '<br>';
+ print_r($resource);
+ echo '</pre>';
+ die('hey');
+*/
+if( isset($_POST['first_name']) && !empty($_POST['first_name']) ){
 
     $first_name = $_POST['first_name'];
 
-} elseif (isset($_POST['firstname']) && !empty($_POST['firstname'])) {
+}elseif ( isset($_POST['firstname']) && !empty($_POST['firstname']) ) {
 
     $first_name = $_POST['firstname'];
 
-} else {
+}else{
 
     $first_name = '';
 
 }
 
-if (isset($_POST['last_name']) && !empty($_POST['last_name'])) {
+if( isset($_POST['last_name']) && !empty($_POST['last_name']) ){
 
     $last_name = $_POST['last_name'];
 
-} elseif (isset($_POST['lastname']) && !empty($_POST['lastname'])) {
+}elseif ( isset($_POST['lastname']) && !empty($_POST['lastname']) ) {
 
     $last_name = $_POST['lastname'];
 
-} else {
+}else{
 
     $last_name = '';
 
 }
 
-if (isset($_POST['phone_number']) && !empty($_POST['phone_number'])) {
+if( isset($_POST['phone_number']) && !empty($_POST['phone_number']) ){
 
     $phone_number = $_POST['phone_number'];
 
-} elseif (isset($_POST['phone']) && !empty($_POST['phone'])) {
+}elseif ( isset($_POST['phone']) && !empty($_POST['phone']) ) {
 
     $phone_number = $_POST['phone'];
 
-} else {
+}else{
 
     $phone_number = '';
 
 }
 
-if (isset($_POST['message']) && !empty($_POST['message'])) {
+if( isset($_POST['message']) && !empty($_POST['message']) ){
 
     $message = $_POST['message'];
 
-} elseif (isset($_POST['description']) && !empty($_POST['description'])) {
+}elseif ( isset($_POST['description']) && !empty($_POST['description']) ) {
 
     $message = $_POST['description'];
 
-} else {
+}else{
 
     $message = '';
 
 }
+
+// echo $_SERVER['HTTP_REFERER'];
+// $referer = $_SERVER['HTTP_REFERER'];
+
+$utm_source = $_POST['utm_source'] ?? null;
+$utm_medium = $_POST['utm_medium'] ?? null;
+$utm_campaign = $_POST['utm_campaign'] ?? null;
+$utm_term = $_POST['utm_term'] ?? null;
+$utm_content = $_POST['utm_content'] ?? null;
 
 // Collect form data
 $data = [
@@ -125,26 +136,36 @@ $contactData = [
     "lastname" => $data['last_name'],
     "emailaddress1" => $data['email'],
     "telephone1" => $data['phone_number'],
-    "ans_whatareyoulookingfortext" => $data['enquiry_subject'],
+    "ans_whatareyoulookingfortext"  => $data['enquiry_subject'],
+    "ans_brand" => 119020001,
+    // "campaignid" => $utm_campaign ?? null,
+    // "ans_leadsource" => $utm_source ?? null,
+    // "ans_googleadclickid" => 'google add test',
+    // "ans_ipcountrycode" => 'some ip country code',
     // "ans_message" => $data['message'],
 ];
+/*
+* 119020001
+echo '<pre>';
+print_r($contactData);
+echo '</pre>';
+*/
 
 // Avoid sending message if it's empty - on duplicate entries, this will delete the previous message submission
 // and we do not want this as we'll lose data
-if (!empty($data['message'])) {
+if( !empty( $data['message'] ) ){
     $contactData["ans_message"] = $data['message'];
 }
 
 // Adding a First Page Seen to the request, this is going to be temporary
-if (!empty($_SERVER['HTTP_REFERER'])) {
+if( !empty( $_SERVER['HTTP_REFERER'] ) ){
 
     $contactData["ans_firstpageseen"] = $_SERVER['HTTP_REFERER'];
 
 }
 
 // Function to get access token
-function getAccessToken($tokenUrl, $clientId, $clientSecret, $resource)
-{
+function getAccessToken($tokenUrl, $clientId, $clientSecret, $resource) {
     $http = curl_init();
 
     curl_setopt($http, CURLOPT_URL, $tokenUrl);
@@ -179,8 +200,7 @@ function getAccessToken($tokenUrl, $clientId, $clientSecret, $resource)
 }
 
 // Function to send data to Dynamics 365
-function sendToDynamics365($apiUrl, $accessToken, $contactData)
-{
+function sendToDynamics365($apiUrl, $accessToken, $contactData) {
     $http = curl_init();
 
     curl_setopt($http, CURLOPT_URL, $apiUrl);
@@ -210,12 +230,49 @@ function sendToDynamics365($apiUrl, $accessToken, $contactData)
 }
 
 // Function to check if lead/contact exists in Dynamics 365
-function checkExistingLead($apiUrl, $accessToken, $email)
-{
+
+// Function to check if lead/contact exists in Dynamics 365
+function checkExistingLead($apiUrl, $accessToken, $email) {
+    $http = curl_init();
+
+    // Query to check if contact exists by email
+    $queryUrl = $apiUrl . "?\$filter=emailaddress1%20eq%20'".$email."'&\$select=leadid,ans_message";
+
+    curl_setopt($http, CURLOPT_URL, $queryUrl);
+    curl_setopt($http, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($http, CURLOPT_HTTPHEADER, [
+        'Content-Type: application/json',
+        'Authorization: Bearer ' . $accessToken,
+    ]);
+
+    $response = curl_exec($http);
+    $error = curl_error($http);
+    curl_close($http);
+
+    if ($error) {
+        throw new Exception("Curl error: $error");
+    }
+
+    $responseData = json_decode($response, true);
+
+    // If the query returns results, return the first lead's ID and ans_message
+    if (isset($responseData['value']) && count($responseData['value']) > 0) {
+        return [
+            'leadid' => $responseData['value'][0]['leadid'],
+            'ans_message' => $responseData['value'][0]['ans_message'] ?? '' // Return existing message if available
+        ];
+    } else {
+        return false; // No existing lead found
+    }
+}
+
+
+/*
+function checkExistingLead($apiUrl, $accessToken, $email) {
     $http = curl_init();
 
     // $queryUrl = $apiUrl . "/contacts?" . urlencode("\$filter") . "=emailaddress1 eq '" . urlencode($email) . "'"; // Query to check if contact exists by email
-    $queryUrl = $apiUrl . "?\$filter=emailaddress1%20eq%20'" . $email . "'";
+    $queryUrl = $apiUrl . "?\$filter=emailaddress1%20eq%20'".$email."'";
 
     curl_setopt($http, CURLOPT_URL, $queryUrl);
     curl_setopt($http, CURLOPT_RETURNTRANSFER, true);
@@ -241,10 +298,9 @@ function checkExistingLead($apiUrl, $accessToken, $email)
         return false; // No existing lead found
     }
 }
-
+*/
 // Function to update an existing lead/contact in Dynamics 365
-function updateExistingLead($apiUrl, $accessToken, $leadId, $contactData)
-{
+function updateExistingLead($apiUrl, $accessToken, $leadId, $contactData) {
     $http = curl_init();
 
     // Ensure the leadId is correctly formatted
@@ -277,10 +333,11 @@ function updateExistingLead($apiUrl, $accessToken, $leadId, $contactData)
 }
 
 // Main logic
-if (!isset($petname) && !empty($first_name) && !empty($last_name) && !empty($phone_number) && !empty($message)) {
+/*
+if( !isset($petname) && !empty($first_name) && !empty($last_name) && !empty($phone_number) && !empty($message) ){
 
 
-    if ($data['enquiry_subject'] !== 'Work Visa' && $data['enquiry_subject'] !== '0') {
+    if( $data['enquiry_subject'] !== 'Work Visa' && $data['enquiry_subject'] !== '0' ){
 
         try {
             $accessToken = getAccessToken($tokenUrl, $clientId, $clientSecret, $resource);
@@ -316,12 +373,12 @@ if (!isset($petname) && !empty($first_name) && !empty($last_name) && !empty($pho
             echo "Error: " . $e->getMessage();
         }
 
-    } else {
+    }else{
 
         print_r('Work Visa Submission attempt');
     }
 
-} else {
+}else{
 
     // Do nothing as it's a BOT submission
     print_r('illegitimate submission');
@@ -335,8 +392,90 @@ if (!isset($petname) && !empty($first_name) && !empty($last_name) && !empty($pho
     print_r($message);
 
 }
+*/
+// Main logic
+if( !isset($petname) && !empty($first_name) && !empty($last_name) && !empty($phone_number) && !empty($message) ){
+
+    if( $data['enquiry_subject'] !== 'Work Visa' && $data['enquiry_subject'] !== '0' ){
+
+        try {
+            $accessToken = getAccessToken($tokenUrl, $clientId, $clientSecret, $resource);
+
+            // Check if the contact/lead already exists
+            $existingLead = checkExistingLead($apiUrl, $accessToken, $data['email']);
+
+            if ($existingLead) {
+                // Lead exists, get the existing message
+                $lead_id = $existingLead['leadid'];
+                $existingMessage = $existingLead['ans_message'];
+
+                // Get the current timestamp in desired format
+                $timestamp = date('d/m/Y H:i'); // Example: 03/10/2024 08:55
+
+                // Append the new message with a separator
+                $newMessage = "\n(Last updated: $timestamp)\n " . $data['message'];
+                //$combinedMessage = trim($existingMessage . ' ' . $newMessage);
+
+                $combinedMessage = trim($newMessage. ' ' . $existingMessage );
+
+                // Prepare data to update Dynamics
+                $contactData['ans_message'] = $combinedMessage;
+
+                // Update the existing lead with the new message
+                try {
+                    $response = updateExistingLead($apiUrl, $accessToken, $lead_id, $contactData);
+                    echo "Lead updated successfully<br>. Response: <pre>" . $response. '</pre>';
+                } catch (Exception $e) {
+                    echo "Error: " . $e->getMessage();
+                }
+
+            } else {
+                // No existing lead, create a new one
+                $contactData['ans_message'] = $data['message'];
+                sendToDynamics365($apiUrl, $accessToken, $contactData);
+                echo "New lead created successfully.<br>";
+            }
+
+        } catch (Exception $e) {
+            echo "Error: " . $e->getMessage();
+        }
+
+    } else {
+        print_r('Work Visa Submission attempt');
+    }
+
+} else {
+    print_r('illegitimate submission');
+}
+
+Mail::send([], [], function ($message) use ($data, $contactData) {
+
+    $utm_source = $_POST['utm_source'] ?? null;
+
+    if ($contactData['ans_brand'] === 119020001 ) {
+        $contactData['ans_brand'] = 'Investment Visa';
+    }
+    else {
+        $contactData['ans_brand'] = 'Portugal Homes';
+    }
+
+    $referer = $_SERVER['HTTP_REFERER'];
+
+    $message->to('enquiries@investmentvisa.com')
+        //$message->to('paulo.bernardes@portugalhomes.com')
+        ->subject('New form submission for Investment Visa')
+        ->html('<h2>Contact Data for Dynamics 365</h2>
+                <p><strong>First Name:</strong> ' . $contactData['firstname'] . '</p>
+                <p><strong>Last Name:</strong> ' . $contactData['lastname'] . '</p>
+                <p><strong>Email Address:</strong> ' . $contactData['emailaddress1'] . '</p>
+                <p><strong>Source:</strong> ' . $referer . '</p>
+                <p><strong>Phone Number:</strong> ' . $contactData['telephone1'] . '</p>
+                <p><strong>Enquiry Subject:</strong> ' . $contactData['ans_whatareyoulookingfortext'] . '</p>
+                <p><strong>Message:</strong> ' . $data['message'] . '</p>
+                <p><strong>Brand:</strong> ' . $contactData['ans_brand'] . '</p>
+                <p><strong>Utm Source:</strong> ' . $utm_source . '</p>');
+});
 
 // Clean up the request handling
 $kernel->terminate($request, $response);
-
 
