271954d6ac11e5744dc0f72f2be36598eb731c0e
PauloAPBernardes <paulo.bernardes@portugalhomes.com>
2024-10-17 12:26:25 +0100

    fix gtags for greece

--- resources/views/pages/residency-and-citizenship.blade.php
+++ resources/views/pages/residency-and-citizenship.blade.php
@@ -80,8 +80,10 @@
     <!-- Google Tag Manager -->
     <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-N5WRRKQX');</script>
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-N5WRRKQX');
+    </script>
     <!-- End Google Tag Manager -->
 
 
@@ -89,9 +91,9 @@
 <body>
 
 <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="
-https://www.googletagmanager.com/ns.html?id=GTM-N5WRRKQX"
-                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<noscript>
+    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N5WRRKQX" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+</noscript>
 <!-- End Google Tag Manager (noscript) -->
 
 <div class="container-fluid">
@@ -498,8 +500,6 @@ https://www.googletagmanager.com/ns.html?id=GTM-N5WRRKQX"
 <button id="scrollToTopBtn" title="Go to top">
     <img src="/images/GoTopArrow.png">
 </button>
-
-<!-- Google tag (gtag.js) --> <script async src="https://www.googletagmanager.com/gtag/js?id=G-GYDWX32BH1"></script> <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-GYDWX32BH1'); </script>
 
 <script>
 
