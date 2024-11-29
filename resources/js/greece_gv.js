44d372066ca2b23cb0201394c1d1421d46195c60
António Lima <antonio.lima@portugalhomes.com>
2024-11-29 12:59:17 +0000

    Published new page for the Greece GV revamp

--- resources/js/greece_gv.js
+++ resources/js/greece_gv.js
@@ -2,12 +2,12 @@ import 'animate.css';
 
 $(document).ready(function () {
 
+    var bannerForm = $('.generic-form-container');
 
     function bannerLoadAnimation(){
-        
+
         let bannerContent = $('.rollover-element');
         let bannerSubtitle = $('.banner-subtitle');
-        let formElement = $('.generic-form-container');
         // let bannerContentHeight = bannerContent.outerHeight();
 
 
@@ -20,11 +20,11 @@ $(document).ready(function () {
             width: bannerContent.width() // Match the original element's width
         })
         .appendTo('body');
-        
+
         setTimeout(() => {
-            
+
             bannerSubtitle.addClass('animate__animated animate__fadeInLeft');
-            formElement.addClass('animate__animated animate__fadeInRight');
+            bannerForm.addClass('animate__animated animate__fadeInRight');
 
         }, 1500); // Add it temporarily to the DOM
 
@@ -39,6 +39,143 @@ $(document).ready(function () {
 
     }
 
+    // Place the form in the correct top position
+    function formDesktopPosition(){
+
+        let bannerContent = $('.rollover-element');
+        let bannerContentTop = bannerContent.offset().top;
+
+
+        bannerForm.css({'top':bannerContentTop});
+
+        console.log(bannerContentTop);
+
+    }
+
+    // Remove placeholders off a form
+    function removeFormPlaceholders(form) {
+
+        // Ensure the form is a valid jQuery object
+        if (!(form instanceof jQuery) || form.length === 0) {
+            console.error("Invalid form passed. Ensure it's a valid jQuery object.");
+            return;
+        }
+
+        // Remove placeholders from all inputs except textareas
+        form.find('input:not([type="textarea"])').each(function () {
+            $(this).removeAttr('placeholder');
+        });
+
+        // Blank out hidden or disabled <option> elements with empty values
+        form.find('select').each(function () {
+            $(this).find('option').each(function () {
+            const $option = $(this);
+            // Check if the option is hidden, disabled, or selected with an empty value
+            if (
+                $option.val() === '' && // Empty value
+                ($option.is('[hidden]') || $option.is('[disabled]') || $option.is('[selected]'))
+            ) {
+                $option.text(''); // Clear the visible text
+            }
+            });
+        });
+    }
+
+    // Load the Banner Animation
     bannerLoadAnimation();
+
+    // Remove the form's placeholders
+    removeFormPlaceholders(bannerForm);
+
+    // Place the form in the correct top position
+    formDesktopPosition();
+
+    /* Banner Form Submission */
+    $(".generic-form-container").on("submit", function(e){
+
+        e.preventDefault();
+
+        var this_form = $(this).serialize();
+        var this_form_element = $(this);
+
+        let submitButton = this_form_element.find('button[type=submit]');
+
+        if( submitButton.length > 0 ){
+
+            submitButton.html('Submitting... <span id="spinner"><svg version="1.1" id="L9" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 0 0" xml:space="preserve" width="20px" height="20px"><path fill="#fff" d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50"><animateTransform attributeName="transform" attributeType="XML" type="rotate" dur="1s" from="0 50 50" to="360 50 50" repeatCount="indefinite" /></path></svg></span>');
+
+        }
+
+        // Disable the button
+        submitButton.prop('disabled', true);
+
+
+        $.ajax({
+            // url: "/form-submission.php",
+            url: "/form-submission",
+            type: "POST",
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            },
+            data: this_form,
+            success:function(response){
+                if (response){
+                    console.log('success: ' + response);
+                }else{
+                    console.log('error: ' + response);
+                }
+            },
+            error: function error(xhr, status, errorMessage) {
+                console.log("RESPONSE: , error: " + errorMessage);
+            },
+            complete: function() {
+                // Revert the button text and remove the spinner
+                submitButton.html('Book Your Meeting');
+                submitButton.prop('disabled', false);
+                // this_form_element[0].reset();
+                // Redirect the user to the /thankyou page after completion
+
+
+                // window.location.href = "/thank-you";
+
+                if (window.location.hash !== '#debug') {
+                    window.location.href = "/thank-you";
+                }
+            }
+        });
+
+    });
+
+    // Testimonials Slider
+    if( $(".testimonials-wrapper.splide").length ){
+
+        var testimonial_slide = new Splide('.testimonials-wrapper', {
+            // type: 'loop',
+            type: 'slide',
+            perPage: 3,
+            perMove: 1,
+            autoplay: false,
+            interval: 3500,
+            gap:'30px',
+            speed: '500',
+            easing: 'ease',
+            arrows: false,
+            pagination: false,
+            drag: false,
+            breakpoints: {
+                991: {
+                    perPage: 2,
+                    type: 'loop',
+                    drag: true,
+                },
+                768: {
+                    gap:'15px',
+                },
+                576: {
+                    perPage: 1,
+                }
+            }
+        }).mount();
+    }
 
 })
--- resources/views/pages/campaigns/greece_gv.blade.php
+++ resources/views/pages/campaigns/greece_gv.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Investment Visa - Residency and Citizenship</title>
+    <title>Investment Visa - Greece Golden Visa</title>
 
     <link rel="icon" type="image/png" href="/images/favicon32x32.png" sizes="16x16">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -12,11 +12,9 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter+Tight:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-    <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
-    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
 
-    <meta property="og:title" content="Investment Visa - Residency and Citizenship" />
+    {{-- OG Meta Tags --}}
+    <meta property="og:title" content="Investment Visa - Greece Golden Visa" />
     <meta property="og:type" content="website" />
     <meta property="og:description" content="Unlock Your European Dream With the Greece Golden Visa" />
     <meta property="og:image" content="https://campaigns.investmentvisa.com/assets/img/campaigns/greece/GR-Scene-Med-DTP.webp" />
@@ -24,20 +22,13 @@
     <meta property="og:site_name" content="Investment Visa" />
     <meta property="og:locale" content="en_US" />
 
+    {{-- Twitter Card Meta Tags --}}
     <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:description" content="Grant of Residency or Citizenship on the Basis of an Investment" />
     <meta name="twitter:image" content="https://campaigns.investmentvisa.com/assets/img/campaigns/greece/GR-Scene-Med-DTP.webp" />
 
-    <meta property="og:image:width" content="1200" />
-    <meta property="og:image:height" content="630" />
-
     <meta name="description" content="Grant of Residency or Citizenship on the Basis of an Investment" />
-    <meta name="msapplication-TileImage" content="https://campaigns.investmentvisa.com/assets/img/campaigns/greece/GR-Scene-Med-DTP.webp" />
 
-    <meta property="og:video" content="https://campaigns.investmentvisa.com/video/IV-Vid-Site-Story-Generic-Invest-5kbs-Dktp.webm" />
-    <meta property="og:video:type" content="video/webm" />
-    <meta property="og:video:width" content="1200" />
-    <meta property="og:video:height" content="630" />
 
     <script src="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/js/splide.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/css/splide.min.css" rel="stylesheet">
@@ -50,7 +41,7 @@
     <!-- Scripts that will mount the plugin that will add the user's country extension to the phone number input -->
     <script src="/assets/js/country-code-plugin/intlTelInput.js"></script>
     <script src="/assets/js/country-code-plugin/utils.js"></script>
-    {{--<script src="/assets/js/country-code-plugin/tel-input-script.js"></script> --}}
+    <script src="/assets/js/country-code-plugin/tel-input-script.js"></script>
     {{-- moved to script.js --}}
 
     {{-- Includes --}}
@@ -106,7 +97,7 @@
                 <div class="container">
                     <div class="row">
 
-                        <div class="col-md-7">
+                        <div class="col-md-7 banner-column">
 
                             <div class="banner-title">
                                 <h1>Golden Visa. The Best<br> Investment For Your Future</h1>
@@ -120,12 +111,12 @@
             </div>
 
             <div class="container">
-                
-                <div class="col-md-7">
 
-                    <p class="banner-subtitle">
+                <div class="col-md-7 banner-column">
+
+                    <h4 class="banner-subtitle">
                         <strong>With a minimum 250k investment</strong>, you can diversify your portfolio, gain the freedom to travel, and secure a Plan B in a stable economy for you and your family.
-                    </p>
+                    </h4>
 
                 </div>
 
@@ -133,97 +124,101 @@
 
             </div>
 
-            <div class="container">
 
-                {{-- Revamped Form --}}
-                <form id="" class="generic-form-container">
-
-                    <h4 class="title">Get Started Now</h4>
-
-                    <div class="input-container form-row">
-
-                        {{-- First Name --}}
-                        <div class="wrapper">
-                            <label for="first_name">First Name *</label>
-                            <input type="text" name="first_name" required>
-                        </div>
-
-                        {{-- Last Name --}}
-                        <div class="wrapper">
-                            <label for="last_name">Last Name *</label>
-                            <input type="text" name="last_name" required>
-                        </div>
-
-                    </div>
-
-                    <div class="input-container form-row">
-
-                        {{-- Email --}}
-                        <div class="email-input-container">
-
-                            <label for="email_address">Email Address*</label>
-                            <input type="email" id="email_address" name="email_address" class="form-control" placeholder="Email Address*" style="width:100%" required>
-
-                            {{-- Zero Bounce status code --}}
-                            <span class="zerobounce-status">Invalid email</span>
-
-                            {{-- Loading Spinning Icon --}}
-                            <svg class="email-validation-spinner" width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                <style>
-                                    .spinner_qM83{
-                                        animation:spinner_8HQG 1.05s infinite
-                                    }
-                                    .spinner_oXPr{
-                                        animation-delay:.1s
-                                    }
-                                    .spinner_ZTLf{
-                                        animation-delay:.2s
-                                    }
-                                    @keyframes spinner_8HQG{0%,57.14%{animation-timing-function:cubic-bezier(0.33,.66,.66,1);transform:translate(0)}28.57%{animation-timing-function:cubic-bezier(0.33,0,.66,.33);transform:translateY(-6px)}100%{transform:translate(0)}}
-                                </style>
-                                <circle class="spinner_qM83" cx="4" cy="12" r="3"/><circle class="spinner_qM83 spinner_oXPr" cx="12" cy="12" r="3"/><circle class="spinner_qM83 spinner_ZTLf" cx="20" cy="12" r="3"/>
-                            </svg>
-
-                        </div>
-
-                        {{-- Phone Number --}}
-                        <div class="wrapper">
-                            <label for="last_name">Phone*</label>
-                            @include('forms.inputs.phone_number')
-                        </div>
-
-                    </div>
-
-                    <div class="wrapper form-row">
-                        <label for="enquiry_subject">What are you looking for*</label>
-                        @include('forms.inputs.what_are_you_looking_for')
-                    </div>
-
-                    <div class="wrapper form-row">
-                        <label>How much are you willing to invest?*</label>
-                        @include('forms.inputs.investment_amount')
-                    </div>
-
-                    <div class="wrapper form-row">
-                        <label for="message_textarea">Message us</label>
-                        @include('forms.inputs.message')
-                    </div>
-
-                    <span class="visas-disclaimer-revamp">Portugal Homes <b>does not</b> provide Work Visas, Tourism Visas, or Temporary Visas.</span>
-
-                    <div class="button-row">
-
-                        <div class="horizontal-line"></div>
-
-                        <button class="stylize" type="submit">Book Your Meeting</button>
-                    </div>
-
-                    <span class="consent-text">By submitting this form, you confirm that you agree that your data will be used to contact you.</span>
-
-                </form>
-
-            </div>
         </section>
+
+        <div class="container form-container-element">
+
+            {{-- Revamped Form --}}
+            <form id="" class="generic-form-container">
+
+                @csrf
+
+                <h4 class="title">Get Started Now</h4>
+
+                <div class="input-container form-row">
+
+                    {{-- First Name --}}
+                    <div class="wrapper">
+                        <label for="first_name">First Name *</label>
+                        <input type="text" name="first_name" required>
+                    </div>
+
+                    {{-- Last Name --}}
+                    <div class="wrapper">
+                        <label for="last_name">Last Name *</label>
+                        <input type="text" name="last_name" required>
+                    </div>
+
+                </div>
+
+                <div class="input-container form-row">
+
+                    {{-- Email --}}
+                    <div class="email-input-container">
+
+                        <label for="email_address">Email Address*</label>
+                        <input type="email" id="email_address" name="email_address" class="form-control" placeholder="Email Address*" style="width:100%" required>
+
+                        {{-- Zero Bounce status code --}}
+                        <span class="zerobounce-status">Invalid email</span>
+
+                        {{-- Loading Spinning Icon --}}
+                        <svg class="email-validation-spinner" width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <style>
+                                .spinner_qM83{
+                                    animation:spinner_8HQG 1.05s infinite
+                                }
+                                .spinner_oXPr{
+                                    animation-delay:.1s
+                                }
+                                .spinner_ZTLf{
+                                    animation-delay:.2s
+                                }
+                                @keyframes spinner_8HQG{0%,57.14%{animation-timing-function:cubic-bezier(0.33,.66,.66,1);transform:translate(0)}28.57%{animation-timing-function:cubic-bezier(0.33,0,.66,.33);transform:translateY(-6px)}100%{transform:translate(0)}}
+                            </style>
+                            <circle class="spinner_qM83" cx="4" cy="12" r="3"/><circle class="spinner_qM83 spinner_oXPr" cx="12" cy="12" r="3"/><circle class="spinner_qM83 spinner_ZTLf" cx="20" cy="12" r="3"/>
+                        </svg>
+
+                    </div>
+
+                    {{-- Phone Number --}}
+                    <div class="wrapper">
+                        <label for="last_name">Phone*</label>
+                        @include('forms.inputs.phone_number')
+                    </div>
+
+                </div>
+
+                <div class="wrapper form-row">
+                    <label for="enquiry_subject">What are you looking for*</label>
+                    @include('forms.inputs.what_are_you_looking_for')
+                </div>
+
+                <div class="wrapper form-row">
+                    <label>How much are you willing to invest?*</label>
+                    @include('forms.inputs.investment_amount')
+                </div>
+
+                <div class="wrapper form-row">
+                    <label for="message_textarea">Message us</label>
+                    @include('forms.inputs.message')
+                </div>
+
+                <span class="visas-disclaimer-revamp">Portugal Homes <b>does not</b> provide Work Visas, Tourism Visas, or Temporary Visas.</span>
+
+                <div class="button-row">
+
+                    <div class="horizontal-line"></div>
+
+                    <button class="stylize" type="submit">Book Your Meeting</button>
+                </div>
+
+                <span class="consent-text">By submitting this form, you confirm that you agree that your data will be used to contact you.</span>
+
+            </form>
+
+        </div>
 
         {{-- Golden Visa Program Benefits --}}
         <section id="golden-visa-benefits" class="section-inner-padding">
@@ -802,40 +797,45 @@
 
                 <h2 class="title">What Our Clients Say About Us</h2>
 
-                <div class="testimonials-wrapper">
-                    <div class="row">
+                <div class="splide testimonials-wrapper">
+                    {{-- <div class="row"> --}}
+                    <div class="splide__track">
+                        <ul class="splide__list">
 
-                        {{-- Item --}}
-                        <div class="col-md-4 item">
+                            {{-- Item --}}
+                            <div class="splide__slide item">
 
-                            @include('partials.quote_marks')
-                            <h4 class="testimonial-text">Improved my family standing for the future with Investment Visa's advise on Greek real estate.</h4>
+                                @include('partials.quote_marks')
+                                <h4 class="testimonial-text">Improved my family standing for the future with Investment Visa's advise on Greek real estate.</h4>
 
-                            <span class="client-name">Mr. & Ms. Demir</span>
+                                <span class="client-name">Mr. & Ms. Demir</span>
 
-                        </div>
+                            </div>
 
-                        {{-- Item --}}
-                        <div class="col-md-4 item">
-                            
-                            @include('partials.quote_marks')
-                            <h4 class="testimonial-text">20 WORDS - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</h4>
+                            {{-- Item --}}
+                            <div class="splide__slide item">
 
-                            <span class="client-name">Mr. & Ms. Demir</span>
+                                @include('partials.quote_marks')
+                                <h4 class="testimonial-text">20 WORDS - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</h4>
 
-                        </div>
+                                <span class="client-name">Mr. & Ms. Demir</span>
 
-                        {{-- Item --}}
-                        <div class="col-md-4 item">
+                            </div>
 
-                            @include('partials.quote_marks')
-                            <h4 class="testimonial-text">Improved my family standing for the future with Investment Visa's advise on Greek real estate.</h4>
+                            {{-- Item --}}
+                            <div class="splide__slide item">
 
-                            <span class="client-name">Mr. & Ms. Demir</span>
+                                @include('partials.quote_marks')
+                                <h4 class="testimonial-text">Improved my family standing for the future with Investment Visa's advise on Greek real estate.</h4>
 
-                        </div>
+                                <span class="client-name">Mr. & Ms. Demir</span>
 
+                            </div>
+
+                        </ul>
                     </div>
+
+                    {{-- </div> --}}
                 </div>
 
             </div>
@@ -844,9 +844,63 @@
         {{-- Footer --}}
         <footer class="section-inner-padding">
             <div class="container">
+                <div class="row">
 
+                    {{-- Column --}}
+                    <div class="col-md-5">
 
+                        <div class="footer-column">
 
+                            <h5>Stay Connected</h5>
+
+                            <div class="social-media-list">
+
+                                <a>Facebook</a>
+                                <a>Instagram</a>
+                                <a>YouTube</a>
+                                <a>LinkedIn</a>
+                                <a>X</a>
+
+                            </div>
+
+                            <div class="terms-privacy-wrapper">
+
+                                <a>Terms & Conditions</a>
+                                <div class="vertical-separator"></div>
+                                <a>Privacy Policy</a>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                    {{-- Column --}}
+                    <div class="col-md-5">
+
+                        <div class="footer-column">
+
+                            <span class="footer-email">
+                                <a href="mailto:info@investmentvisa.com">info@investmentvisa.com</a>
+                            </span>
+
+                            <div class="country-list">
+
+                                <span>Portugal</span>
+                                <span>UK</span>
+                                <span>Greece</span>
+                                <span>UAE</span>
+                                <span>Hong Kong</span>
+
+                            </div>
+
+                            <span class="trademark">2023 Investment Visa. Investment Visa is part of the Harland & Poston Group.</span>
+
+                        </div>
+
+                    </div>
+
+                </div>
             </div>
         </footer>
 
