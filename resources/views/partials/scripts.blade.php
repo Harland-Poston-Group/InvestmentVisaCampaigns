5cd328e60e9e75e85c1f8f743ff92db753fecb6e
António Lima <antonio.lima@portugalhomes.com>
2024-12-03 11:42:55 +0000

    microsoft ads inclusion

--- resources/views/partials/scripts.blade.php
+++ resources/views/partials/scripts.blade.php
@@ -46,6 +46,9 @@
         </script>
         <!-- End Google Tag Manager -->
 
+        {{-- Microsoft Ads --}}
+        <script>(function(w,d,t,r,u){var f,n,i;w[u]=w[u]||[],f=function(){var o={ti:"187135592", enableAutoSpaTracking: true};o.q=w[u],w[u]=new UET(o),w[u].push("pageLoad")},n=d.createElement(t),n.src=r,n.async=1,n.onload=n.onreadystatechange=function(){var s=this.readyState;s&&s!=="loaded"&&s!=="complete"||(f(),n.onload=n.onreadystatechange=null)},i=d.getElementsByTagName(t)[0],i.parentNode.insertBefore(n,i)})(window,document,"script","//bat.bing.com/bat.js","uetq");</script>
+
     @endif
 
     {{-- FontAwesome --}}
