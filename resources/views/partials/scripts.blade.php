12b28d0bbc929e767bbc5323d0b26600b2d428fd
António Lima <ant.abreulima@gmail.com>
2025-01-09 15:38:28 +0000

    Added Gtag code to landing pages

--- resources/views/partials/scripts.blade.php
+++ resources/views/partials/scripts.blade.php
@@ -78,6 +78,16 @@
             });
         </script>
 
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11285295032"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            
+            gtag('config', 'AW-11285295032');
+        </script>
+
     @endif
 
     {{-- FontAwesome --}}
