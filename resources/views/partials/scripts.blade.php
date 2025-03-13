30896dc64a1f2fa53cefe6daa35ccaaafd6b37a5
António Lima <antonio.lima@portugalhomes.com>
2025-03-13 09:29:58 +0000

    Yandex script added

--- resources/views/partials/scripts.blade.php
+++ resources/views/partials/scripts.blade.php
@@ -89,10 +89,28 @@
         </script>
 
         <!-- Taboola Pixel Code -->
-        <script>
+        {{-- <script>
             _tfa.push({notify: 'event', name: 'lead', id: 1810087});
-        </script>
+        </script> --}}
         <!-- End of Taboola Pixel Code -->
+        <!-- Yandex.Metrika counter -->
+
+        <script type="text/javascript" >
+            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+            ym(100301814, "init", {
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true,
+                webvisor:true
+            });
+        </script>
+        <noscript><div><img src="https://mc.yandex.ru/watch/100301814" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+        <!-- /Yandex.Metrika counter -->
 
     @endif
 
