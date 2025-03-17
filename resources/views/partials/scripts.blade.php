a0fad459fe849464ac9e5f4d87c9058ac3815f79
António Lima <antonio.lima@portugalhomes.com>
2025-03-17 11:35:29 +0000

    Removed tag - asked by Miguel

--- resources/views/partials/scripts.blade.php
+++ resources/views/partials/scripts.blade.php
@@ -79,14 +79,14 @@
         </script>
 
         <!-- Google tag (gtag.js) -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11285295032"></script>
+        <!-- <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11285295032"></script>
         <script>
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
 
             gtag('config', 'AW-11285295032');
-        </script>
+        </script> -->
 
         <!-- Taboola Pixel Code -->
         {{-- <script>
